4f896eddbef2d3648b7598fd2a5607452f869f30
IRenatoBarros <renatofilho9722@gmail.com>
2024-10-26 18:19:12 -0300

    subindo novos métodos

--- src/main/java/com/example/demo/models/Postagem.java
+++ src/main/java/com/example/demo/models/Postagem.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -17,7 +18,7 @@ import java.util.UUID;
 @Document(collection = "postagem")
 public class Postagem {
     @Id
-    private UUID id;
+    private UUID  id;
 
     private UUID arquivoId;
     private UUID perfilId;
@@ -28,12 +29,16 @@ public class Postagem {
 
     public Postagem() {
         this.id = UUID.randomUUID(); // Gerar como String
+        this.likes = new ArrayList<>();
+        this.comentarios = new ArrayList<>();
     }
 
 
     @Getter
     @Setter
     public static class Like {
+        @Id
+        private UUID id;
         private UUID perfilId;
         private Date dataLike;
 
@@ -42,6 +47,8 @@ public class Postagem {
     @Getter
     @Setter
     public static class Comentario {
+        @Id
+        private UUID id;
         private UUID perfilId;
         private String comentario;
 
